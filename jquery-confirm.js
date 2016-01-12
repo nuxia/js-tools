12fe6ab819970d5aff5bd6be345d7b8066b53786
Yannick Snobbert <yannick.snobbert@gmail.com>
2016-01-12 12:36:12 +0100

    Debug chained click event

--- jquery-confirm.js
+++ jquery-confirm.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-    $('[data-confirm]').on('click', function (event) {
+    var handler = function (event) {
         if (!confirm($(this).data('confirm'))) {
             event.preventDefault();
+            event.stopImmediatePropagation();
+        } else {
+            $(this).off('click', handler);
         }
-    });
+    };
+    $('[data-confirm]').on('click', handler);
 });
