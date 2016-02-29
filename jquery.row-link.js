9c6e50cbd7cbf824172c915ab4a3efd2ea1eb6f5
Yannick Snobbert <yannick.snobbert@gmail.com>
2016-02-29 16:14:20 +0100

    row-link helper

--- jquery.row-link.js
+++ jquery.row-link.js
@@ -1,21 +1,23 @@
-if ($('table a.row-link').exist()) {
-    $('table tbody td:not(.actions)').each(function () {
-        var $this = $(this),
-            $link = $this.parent('tr').find('a.row-link');
-
-        if ($link.exist()) {
-            $this.addClass('pointer');
-            $this.on('click', function (e) {
-                if ($link.attr('target') === '_blank') {
-                    window.open($link.attr('href'));
-                } else {
-                    window.location = $link.attr('href');
-                }
-            });
-        }
-    });
-
-    $('table tbody tr a').click(function (e) {
-        e.stopPropagation();
-    });
-}
+$(document).ready(function () {
+    if ($('table a.row-link').exist()) {
+        $('table tbody td:not(.actions)').each(function () {
+            var $this = $(this),
+                $link = $this.parent('tr').find('a.row-link');
+    
+            if ($link.exist()) {
+                $this.addClass('pointer');
+                $this.on('click', function (e) {
+                    if ($link.attr('target') === '_blank') {
+                        window.open($link.attr('href'));
+                    } else {
+                        window.location = $link.attr('href');
+                    }
+                });
+            }
+        });
+    
+        $('table tbody tr a').click(function (e) {
+            e.stopPropagation();
+        });
+    }
+});
